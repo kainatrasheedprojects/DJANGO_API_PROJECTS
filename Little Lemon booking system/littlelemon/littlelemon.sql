23d5491d003f39fb41d7f0c09b14e72b9006520c
kainat rasheed <kainatrasheed9211@mail.com>
2023-11-20 06:34:06 -0600

    first  commit

---
+++ Little Lemon booking system/littlelemon/littlelemon.sql
@@ -0,0 +1,2 @@
+create table menu ( id int , name varchar(255),  price decimal, category varchar(255) , category_name varchar(255)) ; 
+INSERT INTO menu (id , name, price, category, category_name ) VALUES ( 11, 'italian pizza' , 400.0, 'italian' , 'stuffed pizza');
--- Little Lemon booking system/littlelemon/settings.py
+++ Little Lemon booking system/littlelemon/settings.py
@@ -39,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # The settings for app updated for the Graded assessment
     'restaurant',
+    'djoser',
+    'rest_framework.authtoken',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -79,11 +82,11 @@ WSGI_APPLICATION = 'littlelemon.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'reservations',
+        'NAME': 'littlelemon',
         'HOST' : '127.0.0.1',
         'PORT' : '3306',
         'USER' : 'root',
-        'PASSWORD' : 'root@123',
+        'PASSWORD' : 'password',
     }
 }
 
@@ -135,3 +138,27 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ),
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    ],
+    'DEFAULT_FILTER_BACKENDS': [
+        'rest_framework.filters.OrderingFilter',
+        'rest_framework.filters.SearchFilter',
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination','PAGE_SIZE': 2,
+    
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '2/minute',
+        'user': '5/minute',
+    }    
+}
+
+DJOSER = {
+    "USER_ID_FIELD": "username"
+}
